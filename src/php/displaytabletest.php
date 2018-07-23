bcd72acd08f9fb01f69ffdf8fc381b07dc9363e4
Matthew James <mja90a@Matthews-MacBook-Pro.local>
2018-07-23 15:15:50 -0400

    update to html

--- src/php/displaytabletest.php
+++ src/php/displaytabletest.php
@@ -1,3 +1,20 @@
+<html>
+<head>
+<title>Display Data into DB</title>
+<style type="text/css">
+table{
+  border: 2px solid red;
+  background-color: #FCC;
+}
+th{
+  border-bottom: 5px solid #000
+}
+td{
+  border-bottom: 2px solid #666
+}
+</style>
+</head>
+<body>
 <h1>Display Data from Table</h1>
 
 <?php
@@ -22,3 +39,6 @@ while($row = mysqli_fetch_array($sqldata, mysqli_assoc)) {
  echo "</table>";
 
 ?>
+
+</body>
+</html>
